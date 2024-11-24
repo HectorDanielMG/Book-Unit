9bd5389123fca00b1d76653b36614bb02b1bc1a5
Diego Cruz Garduño <91989971+Vinyurchin@users.noreply.github.com>
2024-11-24 05:31:11 +0000

    añadido de componentes de ayuda, perfil y pro, con sus respectivas conexiones a la misma, y el botón para cerrar sesión en la barra lateral (requiere un ajuste menor) y plantilla de ayuda para añadir información

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdministracionComponent } from './administracion/administracion.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ProComponent } from './pro/pro.component';
+import { AyudaComponent } from './ayuda/ayuda.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'administracion', component: AdministracionComponent }
+  { path: 'administracion', component: AdministracionComponent },
+  { path: 'perfil', component: PerfilComponent},
+  { path: 'pro', component: ProComponent},
+  { path: 'ayuda', component: AyudaComponent},
 
 ];
---
+++ src/app/pro/pro.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  templateUrl: './pro.component.html',
+})
+export class ProComponent {
+
+  onSignOut(): void {
+    Swal.fire({
+      title: "¿Estás seguro de cerrar sesión?",
+      text: "Se le regresará a la página de inicio.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, cerrar sesión",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Cerrando sesión...", "", "success").then(() => {
+          window.location.href = "#";
+        });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("Cancelado", "Tu sesión sigue activa.", "info");
+      }
+    });
+  }
+}
